9726c15098bb2ed2ba1d8feef24942f8b02d3cdf
manu <emanuele.pocelli@mail.polimi.it>
2026-05-29 22:45:00 +0300

    added jumping mechanic

--- raccoon-racing/scripts/Car/car.gd
+++ raccoon-racing/scripts/Car/car.gd
@@ -1,5 +1,7 @@
 extends Node2D
+class_name Car
 
+@onready var visual: Node2D = $Visual
 @onready var fl: AnimatedSprite2D = $Visual/Wheels/FL
 @onready var fr: AnimatedSprite2D = $Visual/Wheels/FR
 @onready var character: Sprite2D = $Visual/Char
@@ -7,36 +9,76 @@ extends Node2D
 var map:Map
 
 var current_vehicle: GameData.VehicleType
-var friction:float=0.5
+var friction:float=0
 #state: drifting
-var bs:bool
+var bs:bool=false
 #turn drifting threshold
 var bsWheelLength :int= 80
 #speed drifting threshold
 var bsSpeed:int=4
 #speed
-var speed:Vector2
+var speed:Vector2=Vector2.ZERO
 #speed angle relative to sprite angle (i guess) 
 #>0:turning right, <0: turning left
-var moveAngCar: int
+var moveAngCar: float=0
 #sliding angle, >1: right, 0: left
-var bsf: bool
+var bsf: bool=0
 #threshold for stopping sliding
-var bsClearSpeed:int
+var bsClearSpeed:int=3
 #sliding timer
-var bsex:int
+var bsex:int=0
 #car rotation speed
-var carRotationWheel: float
+var carRotationWheel: float=1
 #car maximum rotation
-var maxRotationWheel: float
+var maxRotationWheel: float=4
 #is on ice?
-var isAtIce: bool
+var isAtIce: bool=false
 #locked state
-var isLock:bool
+var isLock:bool=false
 #car power
-var horse:float
+var horse:float=0.3
 #is the car upside down?
-var isBack:float
+var isBack:bool
+#step movement in frame
+var stepx:float=1
+var stepy:float=1
+#temporary new position, before checking collisions
+var tempx:float=0
+var tempy:float=0
+#height from the ground, flying if >1
+var jumpCurrheight:float=0
+#previous jump height
+var jumpPrevheight:float
+#rolling resistance
+var rollGratingNum:float
+#gliding resistance
+var glideGratingNum:float
+#grass resistance
+var grassGratingNum:float
+#jump threshold (i guess?)
+var heightOverWall:int = 10
+#jump floor height
+var jumpFloorHeight: int=0
+#jump bounciness
+var jumpSpring:float=0.5
+#jump speed
+var jumpspeed:float = 0.05
+#gravity
+var downWeight:float=-0.1
+#z layer for jumping
+var airLayer: int=10
+
+func _ready() -> void:
+    steer_normal()
+    current_vehicle=GameData.current_vehicle
+    #TODO:actual values
+    #rollGratingNum=map.rollGratingNum
+    #glideGratingNum=map.glideGratingNum
+    #grassGratingNum=map.grassGratingNum
+    glideGratingNum = 0.0002
+    rollGratingNum = 0.02
+    grassGratingNum = 0.01
+
 
 func steer_left()->void:
     fr.position=Vector2(16.25,-9.7)
@@ -61,22 +103,32 @@ func steer_normal()->void:
     fl.rotation=0
     character.position=Vector2(0,-4)
     character.rotation=0
-    
-func _ready() -> void:
-    steer_normal()
-    current_vehicle=GameData.current_vehicle
+
     
     
-func _physics_process(_delta: float) -> void:
-    #-1 for left, 1 for right, 0 for none
-    var direction := Input.get_axis("ui_left", "ui_right")
-    
-    if direction > 0:
-        steer_right()
-    elif direction < 0:
-        steer_left()
+func _process(_delta: float) -> void:
+    var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
+    # Handle Steering (X-axis)
+    if input_dir.x > 0:
+        TurnLRight()
+    elif input_dir.x < 0:
+        TurnLeft()
     else:
-        steer_normal()
+        CancelTurn()
+
+    # Handle Throttle/Brake (Y-axis)
+    if input_dir.y < 0:
+        Forward()  # In Godot 2D, negative Y is UP
+    elif input_dir.y > 0:
+        Backward()
+    else:
+        Clearward()
+    Update()
+    #TODO:testing, remove
+    if abs(position.x+130)<50 and abs(position.y+145)<50:
+        print('juuumping')
+        JumpBySpeed(0.5)
         
 
 func Forward()->void:
@@ -97,12 +149,12 @@ func Forward()->void:
         
     if(not bs and bsex <= 0):
         if (isAtIce):
-            speed += Vector2(horse, 0).rotated(rotation) * 0.5
+            speed += Vector2(horse,0).rotated(rotation-PI/2) * 0.5
         else:
-            speed += Vector2(horse, 0).rotated(rotation)
+            speed += Vector2(horse,0).rotated(rotation-PI/2)
 
     # if difference is too large: spawn smoke
-    if get_angle_diff()> deg_to_rad(140) or isLock:
+    if abs(get_angle_diff())> 140 or isLock:
         spawn_smoke('smoke1',true)  
         spawn_smoke('smoke1',false) 
 
@@ -110,21 +162,29 @@ func Backward()->void:
     #is this necessary?
     #this.AllWheel()
     if(not bs and bsex <= 0):
-        speed += -Vector2(horse, 0).rotated(rotation)*0.5
-    if get_angle_diff()<deg_to_rad(40) or isLock:
+        speed += -Vector2(horse, 0).rotated(rotation-PI/2)*0.5
+    if abs(get_angle_diff())<40 or isLock:
         spawn_smoke("smoke1",true)
         spawn_smoke("smoke1",false)
+
+func Clearward()->void:
+      if isHovercraft():
+        pass
+        #TODO: stop engine sound
+        #this.stopHCRunSound();
+
+
 
 func TurnLeft()->void:
     if(not isLock):
         if(not isBack):
-            if get_angle_diff()<deg_to_rad(145):
+            if abs(get_angle_diff())<145:
                 rotation_degrees += -min(speed.length() * carRotationWheel,maxRotationWheel)
             #facing downwards:
             else:
                isBack = true;
                rotation_degrees += min(speed.length() * carRotationWheel / 2,maxRotationWheel)
-        elif get_angle_diff()<deg_to_rad(60): 
+        elif abs(get_angle_diff())<60: 
             isBack = false;
             rotation_degrees += - min(speed.length() * carRotationWheel,maxRotationWheel)
         else:
@@ -134,13 +194,13 @@ func TurnLeft()->void:
 func TurnLRight()->void:
     if(not isLock):
         if(not isBack):
-            if get_angle_diff()<deg_to_rad(145):
+            if abs(get_angle_diff())<145:
                 rotation_degrees += min(speed.length() * carRotationWheel,maxRotationWheel)
             #facing downwards:
             else:
                isBack = true;
                rotation_degrees += -min(speed.length() * carRotationWheel / 2,maxRotationWheel)
-        elif get_angle_diff()<deg_to_rad(60): 
+        elif abs(get_angle_diff())<60: 
             isBack = false;
             rotation_degrees += min(speed.length() * carRotationWheel,maxRotationWheel)
         else:
@@ -188,7 +248,7 @@ func Update():
 
 #get difference beteween speed angle and sprite angle
 func get_angle_diff()->float:
-    return angle_difference(speed.angle(), rotation)
+    return rad_to_deg(angle_difference(speed.angle(), rotation-PI/2))
 
 #spawn smoke particle
 func spawn_smoke(type:String, direction:bool)->void:
@@ -204,19 +264,91 @@ func Water()->void:
 
 #update car position
 func UpdateCarPos()->void:
-    pass
+    GetHitCar()
+    stepx=snapped(speed[0],0.1)
+    stepy=snapped(speed[1],0.1)
+    tempx = position.x + stepx
+    tempy = position.y + stepy
+    GetGrassStatus(tempx,tempy)
+    GetHitEvent(tempx,tempy)
+    GetHitStatus(tempx,tempy)
+    position.x = tempx
+    position.y = tempy
 
 #update camera:is it necessary??
 func UpdateViewMap()->void:
     pass
 
 func UpdateSpeed()->void:
-    pass
+    var dir:int
+    if(jumpCurrheight < 1):
+        moveAngCar = (get_angle_diff())
+        friction = abs(moveAngCar)
+        if(friction > 90):
+            friction = 180 - friction;
+        var dragFactor:float=rollGratingNum + int(friction) * glideGratingNum
+        speed *= max(0.0, 1.0 - dragFactor)
+        if(moveAngCar < 90 and moveAngCar > 0 or moveAngCar < -90):
+            dir = -1;
+        else:
+            dir = 1;
+        #calculate magnitude and direction of force
+        var slide_magnitude: float = speed.length() * (glideGratingNum * int(90 - friction))
+        var slide_force: Vector2 = speed.orthogonal() * dir
+        slide_force = slide_force.normalized() * slide_magnitude
 
+        #apply the force
+        speed += slide_force
+
+
+#jump mechanic related to speed
+func JumpBySpeed(height:float)->void:
+    if(jumpCurrheight < heightOverWall):
+        jumpCurrheight += height * speed.length() * jumpspeed;
+        #TODO: add sound 
+        #this.playFastSound()
+        #this.playHCJumpSound()
+        #this.playJumpSound()
+        
+func Jump(height:float)->void:
+    jumpPrevheight = jumpCurrheight
+    jumpCurrheight += height
+    #TODO: add sound
+    #this.playHCJumpSound()
+    
+    
 func Jumping()->void:
-    pass
+    var temp_height:float = jumpCurrheight
+    jumpCurrheight += jumpCurrheight - jumpPrevheight + downWeight
+    if(jumpCurrheight > 1):
+        z_index=airLayer
+    #TODO: should it be removed?
+    else:
+        z_index=0
+    if(jumpCurrheight < jumpFloorHeight):
+        if(jumpFloorHeight - jumpCurrheight > 0.5):
+            pass
+            #TODO: add sound
+            #this.playHCEndJumpSound()
+        jumpCurrheight = (- jumpCurrheight) * jumpSpring
+        jumpPrevheight = (- temp_height) * jumpSpring
+    else:
+        jumpPrevheight = temp_height
+    jumpFloorHeight = 0 
+    var visual_scale = 1.0 + (jumpCurrheight * 0.0175)
+    visual.scale = Vector2(visual_scale, visual_scale)
+
 
 func Loopsounds()->void:
+    pass
+
+func GetHitCar()->void:
+    pass
+func GetGrassStatus(tx:float, ty:float)->void:
+    pass
+func GetHitEvent(tx:float, ty:float)->void:
+    pass
+func GetHitStatus(tx:float, ty:float)->void:
     pass
 
 #is race type hovercraft?
--- raccoon-racing/scripts/map/map.gd
+++ raccoon-racing/scripts/map/map.gd
@@ -2,7 +2,9 @@ extends Node2D
 class_name Map
 
 var scaledTimes:float
-
+var glideGratingNum:float = 0.0002
+var rollGratingNum:float = 0.02
+var grassGratingNum:float = 0.01
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
