66bf59cf8a7a6aefa034902fab0f95ab0d27d4f2
manu <emanuele.pocelli@mail.polimi.it>
2026-07-01 00:55:15 +0300

    finished car points for last map

--- raccoon-racing/scripts/map/map_04.gd
+++ raccoon-racing/scripts/map/map_04.gd
@@ -4,6 +4,8 @@ extends Map
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	ScaledTimes=0.02857142857142857
+	offsetcar=Vector2(76.5,-44.5)
+	offsethc=Vector2(395,168)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
