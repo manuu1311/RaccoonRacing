3dd91d12ed3aed1744f1df889419af87ece54af7
manu <emanuele.pocelli@mail.polimi.it>
2026-07-23 00:39:22 +0300

    fixed invincible infinite duration bug

--- raccoon-racing/scripts/Car/Props/invincible.gd
+++ raccoon-racing/scripts/Car/Props/invincible.gd
@@ -8,13 +8,13 @@ var tickactivate:int
 func _init(playerinst:Player)->void:
 	super(playerinst);
 	proptype = 1;
-	if(player.PlayerID == 0 && !player.car.isInvincible):
+	if(player.PlayerID == GameData.FocusPlayer.PlayerID && !player.car.isInvincible):
 		MusicPlayer.PlayMusic("invincible")
 	player.prop.del_prop_by_type(proptype);
 	player.car.isInvincible = true
 	player.car.prop_effector.AddInvincible(1.5)
 	player.IsUsingProp=true
-	tickactivate=NetworkTime.tickrate+UseTime*NetworkTime.tickrate
+	tickactivate=NetworkTime.tick+NetworkTime.seconds_to_ticks(UseTime)
 	delme()
 
 
@@ -26,7 +26,7 @@ func delme()->void:
 		return
 	if is_instance_valid(player.car):
 		player.IsUsingProp=false
-		if(player.PlayerID==0):
+		if(player.PlayerID==GameData.FocusPlayer.PlayerID):
 			MusicPlayer.PlayMusic("map"+str(GameData.currentMap))
 		player.prop.Delprop(self);
 	
