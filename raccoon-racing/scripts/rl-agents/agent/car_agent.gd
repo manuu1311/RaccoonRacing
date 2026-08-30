ece800c44c7a90022c36670e12617cd8af2d9425
manu <emanuele.pocelli@mail.polimi.it>
2026-08-30 18:32:30 +0300

    improved raycast sensors and added text debug
    improved raycast calculation efficiency, raycasts now only detect one layer, normal wall detection spans all directions, jump wall detection only forward at 0, -15 and 15 degrees; added debug text to visualise values in-game, added distance log-normalisation, to give more weight to close collisions, values rescaled between -1 and 1

--- raccoon-racing/scripts/rl-agents/agent/car_agent.gd
+++ raccoon-racing/scripts/rl-agents/agent/car_agent.gd
@@ -3,8 +3,74 @@ class_name CarAgent
 
 #region export variables
 @export var car:Car
+@export var sensors:CustomRaycastObs
+## offset to account for car shape
+@export var offset:=30
+@export_group("Debug Settings")
+@export var debug_rays:bool=true
+@export var debug_wall_color:Color=Color.RED
+@export var debug_jumpwall_color:Color=Color.YELLOW
+@export var debug_empty_color:Color=Color.GREEN
+@export var debug_ray_width:int=2
+
 #endregion
+
+var sensor_output:PackedFloat32Array
+var _max_observed:float=0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+func _process(_delta: float) -> void:
+	sensor_output=sensors.get_observation()
+	if debug_rays:
+		queue_redraw()
+
+func _normalize_dist(dist: float, max_dist: float) -> float:
+	var clamped_dist:float = clampf(dist-offset, 0.0, max_dist)
+	var log_0_to_1: float = log(1.0 + clamped_dist) / log(1.0 + max_dist)
+	
+	# Remap from [0.0, 1.0] to [-1.0, 1.0]
+	return (log_0_to_1 * 2.0) - 1.0
+
+
+func _draw() -> void:
+	var font: Font = ThemeDB.fallback_font
+	var font_size: int = 12
+	
+	for i in range(0, sensor_output.size(), 5):
+		var is_colliding: float = sensor_output[i]
+		var hit_distance: float = sensor_output[i + 1]
+		var ray_length: float = sensor_output[i + 2]
+		var collision_type: float = sensor_output[i + 3]
+		var ray_angle: float = sensor_output[i + 4]-90
+		var processed_distance:float=0.0
+
+		var dir: Vector2 = Vector2.RIGHT.rotated(car.global_rotation + deg_to_rad(ray_angle))
+		
+		var end_point: Vector2
+		var text_val: String
+		var line_color: Color
+		
+		if is_colliding < 0.5:
+			if collision_type >= 0.5:
+				# skip non-colliding jumpwalls
+				continue
+			end_point = car.position + dir * ray_length
+			line_color = debug_empty_color
+			text_val = ''
+		else:
+			end_point = car.position + dir * hit_distance
+			line_color = debug_jumpwall_color if collision_type >= 0.5 else debug_wall_color
+			#text_val = "%.1f" % (hit_distance/ray_length)
+			processed_distance=_normalize_dist(hit_distance,ray_length)
+			text_val = "%.1f" % (processed_distance)
+			if processed_distance<_max_observed:
+				_max_observed=processed_distance
+			
+		# Draw the ray line
+		draw_line(car.position, end_point, line_color, debug_ray_width)
+		
+		var text_position: Vector2 = end_point + dir * 8.0
+		draw_string(font, text_position, text_val, HORIZONTAL_ALIGNMENT_CENTER, -1, font_size, line_color)
--- raccoon-racing/scripts/rl-agents/agent/raycast_sensors.gd
+++ raccoon-racing/scripts/rl-agents/agent/raycast_sensors.gd
@@ -37,6 +37,15 @@ class_name CustomRaycastObs
 		rays_angles = value
 		_update()
 
+@export var rays_jump_angles := [
+	0,15,-15
+	]:
+	get:
+		return rays_jump_angles
+	set(value):
+		rays_jump_angles = value
+		_update()
+
 @export_range(5, 3000, 5.0) var ray_length := 200:
 	get:
 		return ray_length
@@ -58,13 +67,15 @@ class_name CustomRaycastObs
 		debug_draw = value
 		_update()
 
-var rays := []
+var rays :Array[RayCast2D]= []
+var jump_rays :Array[RayCast2D]= []
 
+var _obs_buffer: PackedFloat32Array
+var _wall_count: int
+var _jump_count: int
 
 func _update()->void:
-	print('updating..')
 	if Engine.is_editor_hint():
-		print('hinting')
 		if debug_draw:
 			_spawn_nodes()
 		else:
@@ -75,12 +86,30 @@ func _update()->void:
 
 func _ready() -> void:
 	_spawn_nodes()
+	_wall_count = len(rays)
+	_jump_count = len(jump_rays)
+	_obs_buffer = PackedFloat32Array()
+	_obs_buffer.resize((_wall_count + _jump_count) * 5)
+
+	_init_static_fields(rays, rays_angles, 0.0, 0)
+	_init_static_fields(jump_rays, rays_jump_angles, 1.0, _wall_count)
+
+# Write the constant parts (max_dist, angle) exactly once.
+func _init_static_fields(ray_arr: Array[RayCast2D], angles: PackedFloat32Array, layer:float, base_index: int) -> void:
+	for i: int in len(ray_arr):
+		var offset: int = (base_index + i) * 5
+		_obs_buffer[offset + 2] = ray_arr[i].target_position.length()
+		_obs_buffer[offset + 3] = layer
+		_obs_buffer[offset + 4] = angles[i]
 
 
 func _spawn_nodes()->void:
 	for ray:RayCast2D in rays:
 		ray.queue_free()
+	for ray:RayCast2D in jump_rays:
+		ray.queue_free()
 	rays = []
+	jump_rays = []
 
 	for angle:int in rays_angles:
 		angle-=90
@@ -100,47 +129,90 @@ func _spawn_nodes()->void:
 		ray.collision_mask = collision_mask
 		add_child(ray)
 		rays.append(ray)
+		
+	for angle:int in rays_jump_angles:
+		angle-=90
+		var ray: = RayCast2D.new()
+		var length:=ray_length
+		# double length of rays within 50 degrees forward
+		# useful for straights
+		if abs(angle+90)<ray_angle_threshold:
+			length*=2
+		ray.set_target_position(
+			Vector2(length * cos(deg_to_rad(angle)), length * sin(deg_to_rad(angle)))
+		)
+		ray.set_name("node_" + str(len(rays))+'_jump')
+		ray.enabled = false
+		ray.collide_with_areas = collide_with_areas
+		ray.collide_with_bodies = collide_with_bodies
+		ray.collision_mask = 1<<2
+		add_child(ray)
+		jump_rays.append(ray)
 
 
-
-func get_observation() -> Array:
+## Computes and returns the flattened array of raycast observations.
+## [br]
+## [b]Output structure:[/b]
+## [br]
+## Each ray contributes a block of 5 floats:
+## [br]
+## - [b]Is Colliding:[/b] 1.0 if colliding, 0.0 otherwise.
+## [br]
+## - [b]Hit Distance:[/b] Distance to collision point.
+## [br]
+## - [b]Ray length:[/b] Length of the ray cast.
+## [br]
+## - [b]Collision Type:[/b] 0.0 for normal walls, 1.0 for jumpable walls.
+## [br]
+## - [b]Ray angle:[/b] Angle of the ray cast wrt the origin.
+## [br]
+## Returns a [PackedFloat32Array].
+func get_observation() -> PackedFloat32Array:
 	return self.calculate_raycasts()
 
 
 func calculate_raycasts() -> PackedFloat32Array:
-	var result:= PackedFloat32Array()
-	var offset:int
-	var max_dist: float
-	for i: int in len(rays):
-		var ray: RayCast2D = rays[i]
+	_update_ray_group(rays, 0)
+	_update_ray_group(jump_rays, _wall_count)
+	return _obs_buffer
+
+func _update_ray_group(ray_arr: Array[RayCast2D], base_index: int) -> void:
+	for i: int in len(ray_arr):
+		var ray: RayCast2D = ray_arr[i]
 		ray.force_raycast_update()
-		offset = i * 4
-		max_dist = ray.target_position.length()
-		result[offset + 2] = max_dist
-		
+		var offset: int = (base_index + i) * 5
+
 		if ray.is_colliding():
 			var hit_point: Vector2 = ray.get_collision_point()
-			var collider: CollisionObject2D = ray.get_collider()
-			
-			result[offset] = 1.0 # True
-			result[offset + 1] = ray.global_position.distance_to(hit_point)
-			#normal wall
-			if collider.collision_layer==1<<1:
-				result[offset + 3] = 0.0
-			#jump wall
-			else:
-				result[offset + 3] = 1.0
+			_obs_buffer[offset] = 1.0
+			_obs_buffer[offset + 1] = ray.global_position.distance_to(hit_point)
+
 		else:
-			result[offset] = 0.0 # False
-			result[offset + 1] = max_dist
-			result[offset + 3] = 0.0
-	return result
+			_obs_buffer[offset] = 0.0
+			_obs_buffer[offset + 1] = _obs_buffer[offset + 2]
 
 
-func _get_raycast_distance(ray: RayCast2D) -> float:
-	if !ray.is_colliding():
-		return 0.0
-
-	var distance := (global_position - ray.get_collision_point()).length()
-	distance = clamp(distance, 0.0, ray_length)
-	return (ray_length - distance) / ray_length
+#func _calculate_ray_arrays(ray_arr:Array[RayCast2D],angles:Array[float],layer:float)->PackedFloat32Array:
+	#var result:= PackedFloat32Array()
+	#result.resize(len(ray_arr)*5)
+	#var offset:int
+	#var max_dist: float
+	#for i: int in (len(ray_arr)):
+		#var ray: RayCast2D = ray_arr[i]
+		#ray.force_raycast_update()
+		#offset = i * 5
+		#max_dist = ray.target_position.length()
+		#result[offset + 2] = max_dist
+		#result[offset+4]=angles[i]
+		#
+		#if ray.is_colliding():
+			#var hit_point: Vector2 = ray.get_collision_point()
+			#
+			#result[offset] = 1.0 # True
+			#result[offset + 1] = ray.global_position.distance_to(hit_point)
+			#result[offset + 3] = layer
+		#else:
+			#result[offset] = 0.0 # False
+			#result[offset + 1] = max_dist
+			#result[offset + 3] = 0.0
+	#return result
